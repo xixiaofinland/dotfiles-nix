6995eb2944852904b6ede8c625e3e6ce34d1e65f
xi xiao <tdxiaoxi2@gmail.com>
2024-06-16 16:33:19 +0000

    +

--- configuration.nix
+++ configuration.nix
@@ -6,9 +6,19 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # zsh as default shell
   users.defaultUserShell = pkgs.zsh;
   environment.shells = with pkgs; [ zsh ];
   programs.zsh.enable = true;
-  
+
+  # optimize space
+  boot.loader.systemd-boot.configurationLimit = 10;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 1w";
+  };
+  nix.settings.auto-optimise-store = true;
+
   system.stateVersion = "24.05";
 }
