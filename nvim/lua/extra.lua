e415940a397d5e25ae35362389f272fb2a07737a
xi xiao <tdxiaoxi2@gmail.com>
2024-06-16 16:50:29 +0000

    +

--- nvim/lua/extra.lua
+++ nvim/lua/extra.lua
@@ -19,26 +19,28 @@ local note_path = vim.fn.expand("~/notes/")
 vim.keymap.set("n", "<leader>no", ":vs " .. note_path .. "personal.md<CR>", { desc = "open personal note" })
 vim.keymap.set("n", "<leader>nw", ":vs " .. note_path .. "work.md<CR>", { desc = "open work note" })
 
-local push_cmd = "cd " .. note_path .. "; git commit -am \"+\"; git push;"
-vim.api.nvim_create_autocmd({ "VimLeave" }, {
-  callback = function()
-    vim.fn.jobstart(push_cmd, { detach = true })
-  end,
-})
+if vim.loop.fs_stat(note_path) ~= nil then
+  local push_cmd = "cd " .. note_path .. "; git commit -am \"+\"; git push;"
+  vim.api.nvim_create_autocmd({ "VimLeave" }, {
+    callback = function()
+      vim.fn.jobstart(push_cmd, { detach = true })
+    end,
+  })
 
-local pull_cmd = "cd " .. note_path .. "; git pull;"
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-  callback = function()
-    vim.fn.jobstart(pull_cmd, {
-      stdout_buffered = true,
-      on_exit =
-          function(_, code)
-            if code == 0 then
-              vim.notify('note pull success!', vim.log.levels.INFO)
-            else
-              vim.notify('note pull failed?', vim.log.levels.ERROR)
-            end
-          end,
-    })
-  end,
-})
+  local pull_cmd = "cd " .. note_path .. "; git pull;"
+  vim.api.nvim_create_autocmd({ "VimEnter" }, {
+    callback = function()
+      vim.fn.jobstart(pull_cmd, {
+        stdout_buffered = true,
+        on_exit =
+            function(_, code)
+              if code == 0 then
+                vim.notify('note pull success!', vim.log.levels.INFO)
+              else
+                vim.notify('note pull failed?', vim.log.levels.ERROR)
+              end
+            end,
+      })
+    end,
+  })
+end
