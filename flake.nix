8b7a8ee7e9f394c63d6e86e26ebbc05d8531d6b9
xi xiao <tdxiaoxi2@gmail.com>
2024-06-17 18:30:53 +0300

    +

--- flake.nix
+++ flake.nix
@@ -4,13 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/nixos-wsl";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, home-manager, nixpkgs, nixos-wsl, }@inputs: {
+  outputs = { self, home-manager, nixpkgs, nixos-wsl, nix-darwin }@inputs: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -21,11 +25,15 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-
             home-manager.users.nixos = import ./home.nix;
           }
         ];
       };
     };
-  };
+
+    darwinConfigurations = {
+      "Xis-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+        modules = [ ./configuration.nix ];
+      };
+    };
 }
