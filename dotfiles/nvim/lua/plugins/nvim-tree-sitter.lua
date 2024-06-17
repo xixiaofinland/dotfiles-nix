6f42c8c751105aa16a74cc18748cb713fe925792
xi xiao <tdxiaoxi2@gmail.com>
2024-06-17 21:30:47 +0300

    +

--- dotfiles/nvim/lua/plugins/nvim-tree-sitter.lua
+++ dotfiles/nvim/lua/plugins/nvim-tree-sitter.lua
@@ -2,7 +2,13 @@ return {
   'nvim-treesitter/nvim-treesitter',
   dependencies = {
     'nvim-treesitter/nvim-treesitter-textobjects',
-    'nvim-treesitter/nvim-treesitter-context'
+    { 'nvim-treesitter/nvim-treesitter-context',
+      config = function()
+        require'treesitter-context'.setup{
+          max_lines = 0,
+        }
+      end
+    },
   },
   build = ':TSUpdate',
   config = function()
