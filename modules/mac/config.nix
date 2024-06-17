26663d7fe4a2acf658f4276cea01827bc7ede5ed
xi xiao <tdxiaoxi2@gmail.com>
2024-06-17 20:07:03 +0300

    +

--- modules/mac/config.nix
+++ modules/mac/config.nix
@@ -7,13 +7,6 @@
   services.nix-daemon.enable = true;
   # nix.package = pkgs.nix;
 
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  # programs.zsh.enable = true;  # default shell on catalina
-  # programs.fish.enable = true;
-
-  # Set Git commit hash for darwin-version.
-  # system.configurationRevision = self.rev or self.dirtyRev or null;
-
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
