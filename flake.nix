dec083e212d11fd200b7734dd0ec83fd74b89f3c
xi xiao <tdxiaoxi2@gmail.com>
2024-06-17 18:57:29 +0000

    + nixos

--- flake.nix
+++ flake.nix
@@ -47,10 +47,6 @@
     mac-user = "xixiao";
     mac-hostname = "Xis-MacBook-Pro";
     mac-sys = "x86_64-darwin";
-
-    linuxSystems = ["x86_64-linux" "aarch64-linux"];
-    darwinSystems = ["aarch64-darwin" "x86_64-darwin"];
-    forAllSystems = f: nixpkgs.lib.genAttrs (linuxSystems ++ darwinSystems) f;
   in {
     nixosConfigurations = {
       "${nixos-hostname}" = nixpkgs.lib.nixosSystem {
@@ -68,27 +64,6 @@
         ];
       };
     };
-
-    # nixosConfigurations = nixpkgs.lib.genAttrs linuxSystems (system: let
-    #   user = "nixos";
-    # in
-    #   nixpkgs.lib.nixosSystem {
-    #     inherit system;
-    #     specialArgs = inputs;
-    #     modules = [
-    #       ./modules/common-config.nix
-    #       ./modules/nixos/config.nix
-    #       nixos-wsl.nixosModules.wsl
-    #       home-manager.nixosModules.home-manager
-    #       {
-    #         home-manager = {
-    #           useGlobalPkgs = true;
-    #           useUserPackages = true;
-    #           users.${user} = import ./home.nix;
-    #         };
-    #       }
-    #     ];
-    #   });
 
     darwinConfigurations = {
       "${mac-hostname}" = nix-darwin.lib.darwinSystem {
@@ -114,52 +89,6 @@
         ];
       };
     };
-
-    # darwinConfigurations = nixpkgs.lib.genAttrs darwinSystems (
-    #   system: let
-    #     user = "xixiao";
-    #   in
-    #     nix-darwin.lib.darwinSystem {
-    #       inherit system;
-    #       specialArgs = inputs;
-    #       modules = [
-    #         home-manager.darwinModules.home-manager
-    #         nix-homebrew.darwinModules.nix-homebrew
-    #         {
-    #           nix-homebrew = {
-    #             inherit user;
-    #             enable = true;
-    #             taps = {
-    #               "homebrew/homebrew-core" = homebrew-core;
-    #               "homebrew/homebrew-cask" = homebrew-cask;
-    #               "homebrew/homebrew-bundle" = homebrew-bundle;
-    #             };
-    #             mutableTaps = false;
-    #             autoMigrate = true;
-    #           };
-    #         }
-    #         # ./hosts/darwin
-    #       ];
-    #     }
-    # );
-
-    # nix code formatter
-    # formatter = forAllSystems (system: {
-    #   ${system} = nixpkgs.legacyPackages.${system}.alejandra;
-    # });
-
-    # devShells = forAllSystems devShell;
-
-    # forAllSystems = f: nixpkgs.lib.genAttrs (linuxSystems ++ darwinSystems) f;
-
-    #   devShell = system: let pkgs = nixpkgs.legacyPackages.${system}; in {
-    #     default = with pkgs; mkShell {
-    #       nativeBuildInputs = with pkgs; [ bashInteractive git ];
-    #       shellHook = with pkgs; ''
-    #         export EDITOR=vim
-    #       '';
-    #     };
-    #   };
 
     formatter.${mac-sys} = nixpkgs.legacyPackages.${mac-sys}.alejandra;
     formatter.${nixos-sys} = nixpkgs.legacyPackages.${nixos-sys}.alejandra;
