620819eb33b334ff847554a08e9edce03f5d79b0
xi xiao <tdxiaoxi2@gmail.com>
2024-06-20 07:39:00 +0000

    nixos

---
+++ modules/mac-config.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  services.nix-daemon.enable = true;
+  system.stateVersion = 4;
+
+  nixpkgs.hostPlatform = "x86_64-darwin";
+
+  nix.gc = {
+    automatic = true;
+    interval = {
+      Weekday = 0;
+      Hour = 0;
+      Minute = 0;
+    };
+    options = "--delete-older-than 14d";
+  };
+}
---
+++ modules/nixos-config.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  wsl.enable = true;
+  wsl.defaultUser = "nixos";
+
+  users.defaultUserShell = pkgs.zsh;
+
+  system.stateVersion = "24.05";
+
+  boot.loader.systemd-boot.configurationLimit = 10;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+}
