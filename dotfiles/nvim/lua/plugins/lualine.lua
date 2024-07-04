b98baa9e3f85d76939fcb727168f857ddaad274c
xi xiao <tdxiaoxi2@gmail.com>
2024-07-04 22:12:36 +0300

    +

--- dotfiles/nvim/lua/plugins/lualine.lua
+++ dotfiles/nvim/lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
   opts = {
     options = {
       icons_enabled = false,
-      theme = 'gruvbox-material',
+      theme = 'tokyonight',
       component_separators = '|',
       section_separators = '',
     },
