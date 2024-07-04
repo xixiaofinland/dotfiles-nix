8f6a0bcdb4658fef2f59a6c8420599103a213f08
xi xiao <tdxiaoxi2@gmail.com>
2024-07-04 21:59:54 +0300

    +

--- dotfiles/nvim/lua/plugins/theme.lua
+++ dotfiles/nvim/lua/plugins/theme.lua
@@ -6,17 +6,23 @@ return {
   --   vim.cmd.colorscheme 'gruvbox-material'
   -- end,
 
-  -- "folke/tokyonight.nvim",
-  -- lazy = false,
-  -- priority = 1000,
-  -- opts = {},
-
-  "rebelot/kanagawa.nvim",
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
   config = function()
-    -- setup must be called before loading
-    vim.cmd("colorscheme kanagawa-wave")
-    -- vim.cmd("colorscheme kanagawa-dragon")
-    -- vim.cmd("colorscheme kanagawa-lotus")
-    vim.api.nvim_set_hl(0, "Visual", {fg='#cc9900', bg='#339966'})
+    vim.cmd[[colorscheme tokyonight-storm]]
+    -- colorscheme tokyonight-night
+    -- colorscheme tokyonight-storm
+    -- colorscheme tokyonight-day
+    -- colorscheme tokyonight-moon
   end
+
+  -- "rebelot/kanagawa.nvim",
+  -- config = function()
+  --   -- setup must be called before loading
+  --   vim.cmd("colorscheme kanagawa-wave")
+  --   -- vim.cmd("colorscheme kanagawa-dragon")
+  --   -- vim.cmd("colorscheme kanagawa-lotus")
+  --   vim.api.nvim_set_hl(0, "Visual", {fg='#cc9900', bg='#339966'})
+  -- end
 }
