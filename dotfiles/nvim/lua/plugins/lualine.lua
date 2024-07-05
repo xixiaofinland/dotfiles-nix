e74e9e56dcd80baf5ed33c8cedd4dbaa0a6cedff
xi xiao <tdxiaoxi2@gmail.com>
2024-07-05 08:02:28 +0300

    +

--- dotfiles/nvim/lua/plugins/lualine.lua
+++ dotfiles/nvim/lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
   opts = {
     options = {
       icons_enabled = false,
-      theme = 'tokyonight',
+      theme = 'tokyonight-storm',
       component_separators = '|',
       section_separators = '',
     },
