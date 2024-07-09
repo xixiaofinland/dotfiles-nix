0d45f82c54da17ce3efe7688b2db808b6a252764
xi xiao <tdxiaoxi2@gmail.com>
2024-07-09 18:16:02 +0300

    +

--- dotfiles/nvim/lua/plugins/markdown.lua
+++ dotfiles/nvim/lua/plugins/markdown.lua
@@ -4,13 +4,16 @@ return {
   config = function()
     require('render-markdown').setup({
       heading = {
-        icons = { '🌟', '🎉 ', '⚡ ', '💡 ', '🔔 ', '🔮 ' },
-        signs = { '' },
-        backgrounds = { 'DiffAdd', 'DiffChange', 'DiffDelete' },
-        foregrounds = { 'markdownH1' },
+        -- icons = { '🌟', '🎉 ', '⚡ ', '💡 ', '🔔 ', '🔮 ' },
+        signs = { '' },   -- remove sign icon
+        backgrounds = { 'DiffAdd', 'DiffChange'},
       },
       bullet = { icons = { '🔸', '🔹', '✅', '☑️' } },
     })
+
+    vim.api.nvim_set_hl(0, 'markdownH1', { fg = '#00FFFF' })
+    vim.api.nvim_set_hl(0, 'markdownH2', { fg = '#00FF00' })
+    vim.api.nvim_set_hl(0, 'markdownH3', { fg = '#FFFF00' })
 
     vim.keymap.set('n', '\\m', require('render-markdown').toggle, { desc = 'markdown' })
   end,
