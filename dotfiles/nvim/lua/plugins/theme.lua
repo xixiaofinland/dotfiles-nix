52bcfad4c217d44c625f0b1e6d01ed5f5563ecf5
xi xiao <tdxiaoxi2@gmail.com>
2024-07-10 15:28:44 +0300

    +

--- dotfiles/nvim/lua/plugins/theme.lua
+++ dotfiles/nvim/lua/plugins/theme.lua
@@ -34,6 +34,6 @@ return {
     vim.cmd("colorscheme kanagawa-wave")
     -- vim.cmd("colorscheme kanagawa-dragon")
     -- vim.cmd("colorscheme kanagawa-lotus")
-    -- vim.api.nvim_set_hl(0, "Visual", { fg = '#cc9900', bg = '#339966' })
+    vim.api.nvim_set_hl(0, "Visual", { fg = '#cc9900', bg = '#339966' })
   end
 }
