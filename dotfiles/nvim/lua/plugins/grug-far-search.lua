a4b5af791e95c6ba3c2880191795ddc1b6eeb8b7
xi xiao <tdxiaoxi2@gmail.com>
2024-07-11 22:05:12 +0300

    +

--- dotfiles/nvim/lua/plugins/grug-far-search.lua
+++ dotfiles/nvim/lua/plugins/grug-far-search.lua
@@ -2,5 +2,18 @@ return {
     'MagicDuck/grug-far.nvim',
     config = function()
         require('grug-far').setup();
+
+        local nmap = function(keys, func, desc)
+            if desc then
+                desc = desc .. ' [grug]'
+            end
+            vim.keymap.set('n', keys, func, { desc = desc })
+        end
+
+        local grup = require('grug-far')
+        nmap('<leader>rg', ':GrugFar<CR>', 'grug open')
+        nmap('<leader>rw', function()
+            grup.grug_far({ prefills = { search = vim.fn.expand("<cword>") } })
+        end, 'grug current word')
     end
 }
