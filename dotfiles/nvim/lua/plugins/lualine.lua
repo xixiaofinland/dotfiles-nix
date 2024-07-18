5b16a7b5f4be5472893bdf8a8de66ddfa4a58661
xi xiao <tdxiaoxi2@gmail.com>
2024-07-18 19:18:00 +0300

    +

--- dotfiles/nvim/lua/plugins/lualine.lua
+++ dotfiles/nvim/lua/plugins/lualine.lua
@@ -2,8 +2,8 @@ return {
   'nvim-lualine/lualine.nvim',
   config = function()
     local function sf_status()
-      local target_org = require 'sf'.get_target_org()
-      local covered_percent = require 'sf'.covered_percent()
+      local target_org = require('sf').get_target_org()
+      local covered_percent = require('sf').covered_percent()
       return target_org .. "(" .. covered_percent .. ")"
     end
 
