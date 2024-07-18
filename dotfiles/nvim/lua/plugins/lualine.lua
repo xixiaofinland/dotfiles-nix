644b5d0791d40afa1bd37b5c97e269629a2ca817
xi xiao <tdxiaoxi2@gmail.com>
2024-07-18 18:55:05 +0300

    +

--- dotfiles/nvim/lua/plugins/lualine.lua
+++ dotfiles/nvim/lua/plugins/lualine.lua
@@ -1,26 +1,29 @@
 return {
   'nvim-lualine/lualine.nvim',
-  opts = {
-    options = {
-      icons_enabled = false,
-      -- theme = 'tokyonight-storm',
-      component_separators = '|',
-      section_separators = '',
-    },
-    sections = {
-      lualine_a = { 'mode' },
-      -- lualine_b = { 'branch' },
-      lualine_b = { 'branch', 'diff', 'diagnostics' },
-      lualine_c = { 'filename', {
-        "require'sf'.get_target_org()",
-        -- cond = function()
-        --   return vim.bo.filetype == 'apex'
-        -- end
-      } },
-      lualine_x = { 'encoding', 'filetype' },
-      -- lualine_x = {'encoding', 'fileformat', 'filetype'},
-      lualine_y = { 'progress' },
-      lualine_z = { 'location' }
+  config = function()
+    local function sf_status()
+      local target_org = require 'sf'.get_target_org()
+      local covered_percent = require 'sf'.covered_percent()
+      return target_org .. "(" .. covered_percent .. ")"
+    end
+
+    require('lualine').setup {
+      options = {
+        icons_enabled = false,
+        -- theme = 'gruvbox',
+        component_separators = '|',
+        section_separators = '',
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        -- lualine_b = { 'branch' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename', sf_status },
+        lualine_x = { 'encoding', 'filetype' },
+        -- lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      }
     }
-  },
+  end
 }
