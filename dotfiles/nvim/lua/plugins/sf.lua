14846932d9f349a6f9da4f62a5782602d8b2d42e
xi xiao <tdxiaoxi2@gmail.com>
2024-07-27 09:50:57 +0300

    +

--- dotfiles/nvim/lua/plugins/sf.lua
+++ dotfiles/nvim/lua/plugins/sf.lua
@@ -1,7 +1,7 @@
 return {
     'xixiaofinland/sf.nvim',
     branch = 'main',
-    -- dir = '~/projects/sf.nvim',
+    dir = '~/projects/sf.nvim',
 
     dependencies = {
         'nvim-treesitter/nvim-treesitter',
@@ -9,9 +9,7 @@ return {
     },
 
     config = function()
-        require('sf').setup({
-            fetch_org_list_at_nvim_start = false,
-        })
+        require('sf').setup({})
 
         vim.keymap.set('n', '<leader>e', '<CMD>e ~/.config/nvim/lua/plugins/sf.lua<CR>',
             { desc = 'Open sf config' })
