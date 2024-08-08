d6c1cca63d38eaa30dafaf78d14d0470f33a5cfd
xi xiao <tdxiaoxi2@gmail.com>
2024-08-08 07:11:32 +0300

    +

--- dotfiles/nvim_config/lua/plugins/theme.lua
+++ dotfiles/nvim_config/lua/plugins/theme.lua
@@ -1,11 +1,4 @@
 return {
-  -- "sainnhe/gruvbox-material",
-  -- priority = 1000,
-  -- config = function()
-  --   vim.o.background = "dark" -- or "light" for light mode
-  --   vim.cmd.colorscheme 'gruvbox-material'
-  -- end,
-
   -- "folke/tokyonight.nvim",
   -- lazy = false,
   -- priority = 1000,
@@ -24,16 +17,24 @@ return {
   --   -- colorscheme tokyonight-moon
   -- end
 
-  "rebelot/kanagawa.nvim",
-  config = function()
-    require('kanagawa').setup({
-      dimInactive = true, -- dim inactive window `:h hl-NormalNC`
-    })
+  -- "rebelot/kanagawa.nvim",
+  -- config = function()
+  --   require('kanagawa').setup({
+  --     dimInactive = true, -- dim inactive window `:h hl-NormalNC`
+  --   })
+  --
+  --   -- setup must be called before loading
+  --   vim.cmd("colorscheme kanagawa-wave")
+  --   -- vim.cmd("colorscheme kanagawa-dragon")
+  --   -- vim.cmd("colorscheme kanagawa-lotus")
+  --   vim.api.nvim_set_hl(0, "Visual", { fg = '#cc9900', bg = '#339966' })
+  -- end
 
-    -- setup must be called before loading
-    vim.cmd("colorscheme kanagawa-wave")
-    -- vim.cmd("colorscheme kanagawa-dragon")
-    -- vim.cmd("colorscheme kanagawa-lotus")
-    vim.api.nvim_set_hl(0, "Visual", { fg = '#cc9900', bg = '#339966' })
+  'ribru17/bamboo.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('bamboo').setup {}
+    require('bamboo').load()
   end
 }
