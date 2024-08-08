eab374bd6c03edb4239e27df40c9a238086a228c
xi xiao <tdxiaoxi2@gmail.com>
2024-08-08 09:13:54 +0300

    +

--- dotfiles/nvim_config/lua/plugins/theme.lua
+++ dotfiles/nvim_config/lua/plugins/theme.lua
@@ -17,24 +17,24 @@ return {
   --   -- colorscheme tokyonight-moon
   -- end
 
-  -- "rebelot/kanagawa.nvim",
-  -- config = function()
-  --   require('kanagawa').setup({
-  --     dimInactive = true, -- dim inactive window `:h hl-NormalNC`
-  --   })
-  --
-  --   -- setup must be called before loading
-  --   vim.cmd("colorscheme kanagawa-wave")
-  --   -- vim.cmd("colorscheme kanagawa-dragon")
-  --   -- vim.cmd("colorscheme kanagawa-lotus")
-  --   vim.api.nvim_set_hl(0, "Visual", { fg = '#cc9900', bg = '#339966' })
-  -- end
-
-  'ribru17/bamboo.nvim',
-  lazy = false,
-  priority = 1000,
+  "rebelot/kanagawa.nvim",
   config = function()
-    require('bamboo').setup {}
-    require('bamboo').load()
+    require('kanagawa').setup({
+      dimInactive = true, -- dim inactive window `:h hl-NormalNC`
+    })
+
+    -- setup must be called before loading
+    vim.cmd("colorscheme kanagawa-wave")
+    -- vim.cmd("colorscheme kanagawa-dragon")
+    -- vim.cmd("colorscheme kanagawa-lotus")
+    vim.api.nvim_set_hl(0, "Visual", { fg = '#cc9900', bg = '#339966' })
   end
+
+  -- 'ribru17/bamboo.nvim',
+  -- lazy = false,
+  -- priority = 1000,
+  -- config = function()
+  --   require('bamboo').setup {}
+  --   require('bamboo').load()
+  -- end
 }
