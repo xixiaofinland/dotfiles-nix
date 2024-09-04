def9699fb240829c7c2ee7b373929641ff39cf62
xi xiao <tdxiaoxi2@gmail.com>
2024-09-04 17:18:17 +0300

    add caches

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
       "xixiaofinland.cachix.org-1:GORHf4APYS9F3nxMQRMGGSah0+JC5btI5I3CKYfKayc="
       "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
       "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
 
