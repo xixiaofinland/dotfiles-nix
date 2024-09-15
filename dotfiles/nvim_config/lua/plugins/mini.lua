9a5052af09df611740a5a70219a9e08af35c78de
xi xiao <tdxiaoxi2@gmail.com>
2024-09-15 10:38:35 +0300

    +

--- dotfiles/nvim_config/lua/plugins/mini.lua
+++ dotfiles/nvim_config/lua/plugins/mini.lua
@@ -54,31 +54,33 @@ return {
         }
       })
 
-      -- local map_split = function(buf_id, lhs, direction)
-      --   local rhs = function()
-      --     -- Make new window and set it as target
-      --     local new_target_window
-      --     vim.api.nvim_win_call(MiniFiles.get_target_window(), function()
-      --       vim.cmd(direction .. ' split')
-      --       new_target_window = vim.api.nvim_get_current_win()
-      --     end)
-      --
-      --     MiniFiles.set_target_window(new_target_window)
-      --   end
-      --
-      --   local desc = 'Split ' .. direction
-      --   vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
-      -- end
+      local map_split = function(buf_id, lhs, direction)
+        local rhs = function()
+          local cur_target = MiniFiles.get_explorer_state().target_window
+          local new_target = vim.api.nvim_win_call(cur_target, function()
+            vim.cmd(direction .. ' split')
+            return vim.api.nvim_get_current_win()
+          end)
+
+          MiniFiles.set_target_window(new_target)
+        end
+
+        local desc = 'Split ' .. direction
+        vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
+      end
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'MiniFilesBufferCreate',
+        callback = function(args)
+          local buf_id = args.data.buf_id
+          map_split(buf_id, '<C-s>', 'belowright horizontal')
+          map_split(buf_id, '<C-v>', 'belowright vertical')
+        end,
+      })
 
       vim.api.nvim_create_autocmd('User', {
         pattern = 'MiniFilesWindowUpdate',
         callback = function(args)
-          local buf_id = args.data.buf_id
-
-          -- open in new window;
-          -- map_split(buf_id, '<C-v>', 'belowright vertical')
-
-          -- show line num in mini.files;
           vim.wo[args.data.win_id].number = true
           vim.wo[args.data.win_id].relativenumber = true
         end,
