a4631bfe4494bc1dcb6fb2f953d9d3bf2ea9e075
xi xiao <tdxiaoxi2@gmail.com>
2024-09-20 09:48:04 +0300

    +

--- modules/home.nix
+++ modules/home.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: let
   finter = pkgs.rustPlatform.buildRustPackage rec {
@@ -17,6 +18,7 @@
       maintainers = with maintainers; ["xixiaofinland"];
     };
   };
+  nvimConfigDir = "${config.home.homeDirectory}/dotfiles-nix/dotfiles/nvim_config";
 in {
   home.packages = with pkgs; [
     cachix
@@ -78,9 +80,24 @@ in {
   # trade impurity for convenience as I need to update nvim config quite frequently!
   xdg.configFile."nvim" = {
     source =
-      config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/dotfiles-nix/dotfiles/nvim_config";
+      config.lib.file.mkOutOfStoreSymlink nvimConfigDir;
     recursive = true;
+  };
+
+  # darwin + home manager doesn't automatically link the new files from Nvim config folder.
+  home.activation = {
+    linkNewNvimFiles = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      echo "Linking new Neovim configuration files..."
+      find "${nvimConfigDir}" -type f | while read file; do
+        relative_path="''${file#${nvimConfigDir}/}"
+        target="$HOME/.config/nvim/$relative_path"
+        if [ ! -e "$target" ]; then
+          $DRY_RUN_CMD mkdir -p "$(dirname "$target")"
+          $DRY_RUN_CMD ln -s $VERBOSE_ARG "$file" "$target"
+          echo "Linked new file: $relative_path"
+        fi
+      done
+    '';
   };
 
   home.sessionVariables = {
