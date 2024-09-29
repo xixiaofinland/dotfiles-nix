a7806cbbff3b104c7b0c9a7693fed9a6a2753a83
xi xiao <tdxiaoxi2@gmail.com>
2024-09-29 22:13:38 +0300

    +

--- dotfiles/nvim_config/lua/plugins/debugprint.lua
+++ dotfiles/nvim_config/lua/plugins/debugprint.lua
@@ -1,5 +1,27 @@
 return {
-    {
-        'andrewferrier/debugprint.nvim',
-    }
+    "andrewferrier/debugprint.nvim",
+    opts = {
+        keymaps = {
+            normal = {
+                plain_below = "<leader>dp",
+                plain_above = "<leader>dP",
+                variable_below = "<leader>dv",
+                variable_above = "<leader>dV",
+                -- variable_below_alwaysprompt = nil,
+                -- variable_above_alwaysprompt = nil,
+                textobj_below = "<leader>do",
+                textobj_above = "<leader>dO",
+                toggle_comment_debug_prints = "<leader>dt",
+                delete_debug_prints = "<leader>dd",
+            },
+            -- visual = {
+            --     variable_below = "g?v",
+            --     variable_above = "g?V",
+            -- },
+        },
+        commands = {
+            toggle_comment_debug_prints = "ToggleCommentDebugPrints",
+            delete_debug_prints = "DeleteDebugPrints",
+        },
+    },
 }
