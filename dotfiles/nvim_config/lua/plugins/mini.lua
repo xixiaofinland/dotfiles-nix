f5c780dd7af73c145c9b5c080ae71c65f5212ec6
xi xiao <tdxiaoxi2@gmail.com>
2024-10-02 18:56:48 +0300

    +

--- dotfiles/nvim_config/lua/plugins/mini.lua
+++ dotfiles/nvim_config/lua/plugins/mini.lua
@@ -88,6 +88,20 @@ return {
         end,
       })
 
+      local set_mark = function(id, path, desc)
+        MiniFiles.set_bookmark(id, path, { desc = desc })
+      end
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'MiniFilesExplorerOpen',
+        callback = function()
+          -- set_mark('c', vim.fn.stdpath('config'), 'Config') -- path
+          -- set_mark('w', vim.fn.getcwd, 'Working directory') -- callable
+          set_mark('a', '~/projects/afmt/tests/static/', 'afmt manual test directory')
+          set_mark('b', '~/projects/afmt/tests/prettier/', 'afmt prettier test directory')
+          set_mark('c', '~/projects/afmt/tests/prettier2/', 'afmt extra test directory')
+        end,
+      })
+
       local minifiles_toggle = function()
         if not MiniFiles.close() then
           MiniFiles.open(vim.api.nvim_buf_get_name(0))
