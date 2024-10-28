08b96b63e0e75be93b27802fd642e101c6064fa7
xi xiao <tdxiaoxi2@gmail.com>
2024-10-28 10:54:35 +0200

    +

--- dotfiles/.tmux_init_actions.sh
+++ dotfiles/.tmux_init_actions.sh
@@ -4,46 +4,46 @@
 
 curl -s https://zenquotes.io/api/random | jq -r '.[0] | "\(.q) - \(.a)"' > "$HOME/.quote"
 
-# # 2. Apex LSP update
-#
-# FILE_URL="https://raw.githubusercontent.com/forcedotcom/salesforcedx-vscode/develop/packages/salesforcedx-vscode-apex/out/apex-jorje-lsp.jar"
-#
-# SAVE_PATH="$HOME/apex-jorje-lsp.jar"
-#
-# LAST_MODIFIED_FILE="$HOME/.apex_lsp_etag" # Etag also defines the resource version as "last modified date" doesn't present in the response
-#
-# get_last_modified() {
-#     curl -sI "$FILE_URL" | grep -i "ETag" | awk '{print $2}'
-# }
-#
-# download_file() {
-#     curl -L -o "$SAVE_PATH" "$FILE_URL"
-# }
-#
-# # Get the current Last-Modified date
-# current_modified=$(get_last_modified)
-#
-# if [ -f "$LAST_MODIFIED_FILE" ]; then
-#     last_modified=$(cat "$LAST_MODIFIED_FILE")
-# else
-#     last_modified=""
-# fi
-#
-# NC='\033[0m' # No Color
-# if [ "$current_modified" != "$last_modified" ]; then
-#     RED='\033[0;31m'
-#     echo -e "${RED}apex-jorje-lsp.jar: downloading new version...${NC}\n"
-#     download_file
-#     echo "$current_modified" > "$LAST_MODIFIED_FILE"
-# else
-#     GREEN='\033[0;32m'
-#     echo -e "${GREEN}apex-jorje-lsp.jar: no change detected.${NC}\n"
-# fi
-#
-# # 3. repos pull
-#
-# echo -e "pull dotfiles... \n"
-# git -C $HOME/dotfiles-nix/ pull
-#
-# echo -e "pull afmt... \n"
-# git -C $HOME/projects/afmt/ pull
+# 2. Apex LSP update
+
+FILE_URL="https://raw.githubusercontent.com/forcedotcom/salesforcedx-vscode/develop/packages/salesforcedx-vscode-apex/out/apex-jorje-lsp.jar"
+
+SAVE_PATH="$HOME/apex-jorje-lsp.jar"
+
+LAST_MODIFIED_FILE="$HOME/.apex_lsp_etag" # Etag also defines the resource version as "last modified date" doesn't present in the response
+
+get_last_modified() {
+    curl -sI "$FILE_URL" | grep -i "ETag" | awk '{print $2}'
+}
+
+download_file() {
+    curl -L -o "$SAVE_PATH" "$FILE_URL"
+}
+
+# Get the current Last-Modified date
+current_modified=$(get_last_modified)
+
+if [ -f "$LAST_MODIFIED_FILE" ]; then
+    last_modified=$(cat "$LAST_MODIFIED_FILE")
+else
+    last_modified=""
+fi
+
+NC='\033[0m' # No Color
+if [ "$current_modified" != "$last_modified" ]; then
+    RED='\033[0;31m'
+    echo -e "${RED}apex-jorje-lsp.jar: downloading new version...${NC}\n"
+    download_file
+    echo "$current_modified" > "$LAST_MODIFIED_FILE"
+else
+    GREEN='\033[0;32m'
+    echo -e "${GREEN}apex-jorje-lsp.jar: no change detected.${NC}\n"
+fi
+
+# 3. repos pull
+
+echo -e "pull dotfiles... \n"
+git -C $HOME/dotfiles-nix/ pull
+
+echo -e "pull afmt... \n"
+git -C $HOME/projects/afmt/ pull
