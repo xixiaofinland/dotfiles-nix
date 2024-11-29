27bdd5068c68640fd140650d41aaa26d7ee09be4
xi xiao <tdxiaoxi2@gmail.com>
2024-11-29 06:30:45 +0200

    +

--- dotfiles/nvim_config/lua/plugins/mini.lua
+++ dotfiles/nvim_config/lua/plugins/mini.lua
@@ -103,6 +103,7 @@ return {
         callback = function()
           set_mark('a', '~/projects/afmt/tests/prettier80/', 'afmt prettier test directory')
           set_mark('b', '~/projects/afmt/tests/static/', 'afmt static test directory')
+          set_mark('c', '~/projects/afmt/tests/to-do/', 'afmt static todo directory')
         end,
       })
 
