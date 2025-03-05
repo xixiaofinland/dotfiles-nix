ec7d7d35e4fd65c61b9d59ddc175709c5aa7af45
xi xiao <tdxiaoxi2@gmail.com>
2025-03-05 16:49:50 +0200

    +

--- modules/home.nix
+++ modules/home.nix
@@ -167,9 +167,19 @@ in {
       set-window-option -g automatic-rename on
       set-option -g automatic-rename-format '#{b:pane_current_path}'
 
-      # This ensures window naming uses the basename while preserving existing formatting
-      set-window-option -g window-status-format '#I:#{b:pane_current_path}#F'
-      set-window-option -g window-status-current-format '#I:#{b:pane_current_path}#F'
+      # default window title colors
+      set-window-option -g window-status-style fg=white
+      set-window-option -g window-status-style bg=default
+      set-window-option -g window-status-style dim
+
+      # active window title colors
+      set-window-option -g window-status-current-style fg=white
+      set-window-option -g window-status-current-style bg=default
+      set-window-option -g window-status-current-style bright
+
+      # BUG: the above defined styles are gone due to the 2 lines below. why?
+      set-window-option -g window-status-format '#I) #{b:pane_current_path}'
+      set-window-option -g window-status-current-format '#I) #{b:pane_current_path}'
 
       # Start windows and panes at 1, not 0
       set -g base-index 1
@@ -199,22 +209,12 @@ in {
       set -sg escape-time 0
 
       # Reload tmux config
-      bind r source-file ~/.config/tmux/.tmux.conf
+      bind r source-file ~/.config/tmux/tmux.conf
 
       # THEME
       set -g status-fg white
       set -g status-bg black
       set -g status-style bright
-
-      # default window title colors
-      set-window-option -g window-status-style fg=white
-      set-window-option -g window-status-style bg=default
-      set-window-option -g window-status-style dim
-
-      # active window title colors
-      set-window-option -g window-status-current-style fg=white
-      set-window-option -g window-status-current-style bg=default
-      set-window-option -g window-status-current-style bright
 
       set -g status-interval 5
       set -g status-left-length 47
