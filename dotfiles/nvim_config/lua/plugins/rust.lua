b1fc2a2ff5b68d8212d9923503fb0631c97486c8
xi xiao <tdxiaoxi2@gmail.com>
2025-04-12 15:32:37 +0300

    +

--- dotfiles/nvim_config/lua/plugins/rust.lua
+++ dotfiles/nvim_config/lua/plugins/rust.lua
@@ -1,7 +1,21 @@
 return {
   {
     'mrcjkb/rustaceanvim',
-    version = '^6', -- Recommended
-    lazy = false,   -- This plugin is already lazy
-  },
+    version = '^6',
+    lazy = false,
+    init = function()
+      vim.g.rustaceanvim = {
+        server = {
+          settings = {
+            ["rust-analyzer"] = {
+              checkOnSave = { command = "clippy" },
+              files = {
+                watcher = "client", -- default "server" doesn't work in macOS
+              },
+            },
+          },
+        },
+      }
+    end,
+  }
 }
