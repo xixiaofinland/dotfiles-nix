ae82d1c2164591496290b06d5eedff4ff13cbc8d
xi xiao <tdxiaoxi2@gmail.com>
2025-04-22 18:26:51 +0300

    +

--- modules/home.nix
+++ modules/home.nix
@@ -309,15 +309,15 @@ in {
         ll = ["log" "-r" "(master..@):: | (master..@)-"];
         lm = ["log" "-r" "mine()"];
         bto = ["bookmark" "track" "glob:*@origin"];
+        pull = [
+          "util"
+          "exec"
+          "--"
+          "bash"
+          "-c"
+          "'jj git fetch && jj rebase -s @ -d trunk()'"
+        ];
       };
-      pull = [
-        "util"
-        "exec"
-        "--"
-        "bash"
-        "-c"
-        "'jj git fetch && jj rebase -s @ -d trunk()'"
-      ];
     };
   };
 
