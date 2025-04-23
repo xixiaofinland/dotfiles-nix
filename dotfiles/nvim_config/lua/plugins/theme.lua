6adb28d794615b19d56290aef677dcdc747d109f
xi xiao <tdxiaoxi2@gmail.com>
2025-04-23 08:08:24 +0300

    +

--- dotfiles/nvim_config/lua/plugins/theme.lua
+++ dotfiles/nvim_config/lua/plugins/theme.lua
@@ -10,4 +10,9 @@ return {
     -- vim.cmd("colorscheme kanagawa-lotus")
     vim.api.nvim_set_hl(0, "Visual", { fg = '#cc9900', bg = '#339966' })
   end
+
+  -- TO-TRY:
+  -- https://github.com/bluz71/vim-moonfly-colors
+  -- https://github.com/bluz71/vim-nightfly-colors
+  -- https://github.com/EdenEast/nightfox.nvim
 }
