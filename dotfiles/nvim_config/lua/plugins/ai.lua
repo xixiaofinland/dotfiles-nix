d6092266ad0658e53e5fed72050fd1c996c53a69
xi xiao <tdxiaoxi2@gmail.com>
2025-04-23 08:32:09 +0300

    +

--- dotfiles/nvim_config/lua/plugins/ai.lua
+++ dotfiles/nvim_config/lua/plugins/ai.lua
@@ -1,11 +1,19 @@
 return {
     {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({})
+        end,
+    },
+    {
         "CopilotC-Nvim/CopilotChat.nvim",
         dependencies = {
             { "zbirenbaum/copilot.lua" },
             { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
         },
-        build = "make tiktoken",                      -- Only on MacOS or Linux
+        build = "make tiktoken",                            -- Only on MacOS or Linux
         opts = {
             -- See Configuration section for options
         },
--- dotfiles/nvim_config/lua/plugins/blink-cmp.lua
+++ dotfiles/nvim_config/lua/plugins/blink-cmp.lua
@@ -1,7 +1,7 @@
 return {
   'saghen/blink.cmp',
   -- optional: provides snippets for the snippet source
-  dependencies = 'rafamadriz/friendly-snippets',
+  dependencies = { 'rafamadriz/friendly-snippets', 'fang2hou/blink-copilot' },
 
   -- use a release tag to download pre-built binaries
   version = '*',
@@ -32,7 +32,15 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
+      providers = {
+        copilot = {
+          name = "copilot",
+          module = "blink-copilot",
+          score_offset = 100,
+          async = true,
+        },
+      },
     },
 
     -- Disable cmdline
