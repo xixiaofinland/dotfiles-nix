934ab2b8ddd3f7181fb2af558722291b8dd9218b
xi xiao <tdxiaoxi2@gmail.com>
2025-05-17 05:25:03 +0300

    +

--- dotfiles/nvim_config/lua/plugins/golf-vim.lua
+++
@@ -1,3 +0,0 @@
-return {
-  "vuciv/golf",
-}
--- dotfiles/nvim_config/lua/plugins/screenkey.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "NStefan002/screenkey.nvim",
-  lazy = false,
-  branch = 'dev',
-  config = function()
-    vim.keymap.set('n', '\\k', ':Screenkey<CR>', { noremap = true, silent = true, desc = 'screenkey' })
-  end
-}
