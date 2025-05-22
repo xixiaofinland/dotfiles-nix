cf9debb73206e4e358e295475bcc4f27a157aa3a
xi xiao <tdxiaoxi2@gmail.com>
2025-05-22 17:07:55 +0300

    +

--- dotfiles/nvim_config/lsp/lua_ls.lua
+++ dotfiles/nvim_config/lsp/lua_ls.lua
@@ -1,5 +1,28 @@
 return {
-  cmd = {'lua-language-server'},
-  filetypes = {'lua'},
-  root_markers = {'.luarc.json', '.luarc.jsonc'},
+  capabilities = vim.lsp.protocol.make_client_capabilities(),
+
+  -- cmd = {'lua-language-server'},
+  -- filetypes = {'lua'},
+  -- root_markers = {'.luarc.json', '.luarc.jsonc'},
+
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+        },
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 }
+
