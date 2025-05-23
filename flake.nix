ddc31c3bbd7889858225329a662c94d383d2268c
xi xiao <tdxiaoxi2@gmail.com>
2025-05-23 23:02:19 +0300

    +

--- flake.nix
+++ flake.nix
@@ -107,10 +107,10 @@
           ./modules/common-config.nix
           ./modules/mac-config.nix
           {
-            users.users.${mac-user} = {
-              shell = pkgs.fish;
-              home = "/Users/${mac-user}";
-            };
+            # users.users.${mac-user} = {
+            #   shell = pkgs.fish;
+            #   home = "/Users/${mac-user}";
+            # };
           }
           home-manager.darwinModules.home-manager
           {
