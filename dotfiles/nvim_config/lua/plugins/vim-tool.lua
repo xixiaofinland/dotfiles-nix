946dc74e83ae5a4b352ab23473a3ae9f4399e8a9
xi xiao <tdxiaoxi2@gmail.com>
2025-07-09 21:40:29 +0300

    +

--- dotfiles/nvim_config/lua/plugins/vim-tool.lua
+++ dotfiles/nvim_config/lua/plugins/vim-tool.lua
@@ -1,19 +1,19 @@
 return {
-  {
-    'szymonwilczek/vim-be-better',
-    config = function()
-      -- Optional: Enable logging for debugging
-      -- vim.g.vim_be_better_log_file = 1
-    end
-  },
+  -- {
+  --   'szymonwilczek/vim-be-better',
+  --   config = function()
+  --     -- Optional: Enable logging for debugging
+  --     -- vim.g.vim_be_better_log_file = 1
+  --   end
+  -- },
 
-  {
-    "m4xshen/hardtime.nvim",
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    opts = {
-      disable_mouse = false,
-    }
-  },
+  -- {
+  --   "m4xshen/hardtime.nvim",
+  --   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  --   opts = {
+  --     disable_mouse = false,
+  --   }
+  -- },
 
   {
     "tris203/precognition.nvim",
@@ -22,9 +22,9 @@ return {
     }
   },
 
-  vim.keymap.set('n', '\\h', function()
-    vim.cmd('Hardtime toggle')
-  end, { noremap = true, silent = true, desc = 'hard-time' }),
+  -- vim.keymap.set('n', '\\h', function()
+  --   vim.cmd('Hardtime toggle')
+  -- end, { noremap = true, silent = true, desc = 'hard-time' }),
 
   vim.keymap.set('n', '\\p', function()
     require("precognition").toggle()
