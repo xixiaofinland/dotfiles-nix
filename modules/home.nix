56a5d2a5479e9d4ade340ae979534394e4ffc4b6
xi xiao <tdxiaoxi2@gmail.com>
2025-07-20 15:39:50 +0300

    +

--- modules/home.nix
+++ modules/home.nix
@@ -187,9 +187,7 @@ in {
       bind -T copy-mode-vi y send -X copy-pipe-and-cancel 'xclip -in -selection clipboard'
 
       bind \' split-window -h -c "#{pane_current_path}"
-      bind \\ split-window -h -c "#{pane_current_path}"
       bind - split-window -v -c "#{pane_current_path}"
-      bind = split-window -v -c "#{pane_current_path}"
       bind c new-window -c "#{pane_current_path}"
       bind q killp
 
