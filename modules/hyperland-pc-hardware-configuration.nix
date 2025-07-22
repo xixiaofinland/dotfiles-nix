148a7b06bd38b0740cfd9d4be5235c47aa9c7511
Xi Xiao <xi.xiao007@gmail.com>
2025-07-22 18:40:37 +0300

    Update and rename hardware-configuration.nix to hyperland-pc-hardware-configuration.nix

--- modules/hyperland-pc-hardware-configuration.nix
+++ modules/hyperland-pc-hardware-configuration.nix
@@ -1,10 +1,6 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
-
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
