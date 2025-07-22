16babf946282df5e769018e3c1ab74b012f63765
Xi Xiao <xi.xiao007@gmail.com>
2025-07-22 22:05:14 +0300

    Update hyprland-config.nix

--- modules/hyprland-config.nix
+++ modules/hyprland-config.nix
@@ -54,56 +54,40 @@
   # ── Fix for some Electron apps in Wayland ─
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
-  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  boot.extraModulePackages = [
+    config.boot.kernelPackages.nvidia_x11
+  ];
+  boot.blacklistedKernelModules = [
+    "nouveau"
+    "rivafb"
+    "nvidiafb"
+    "rivatv"
+    "nv"
+    "uvcvideo"
+  ];
+  services.xserver.videoDrivers = [
+    "nvidia"
+    "intel"
+  ];
+
   hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.latest; # `latest` is `555.58.02` currently
 
-    # Modesetting is required.
     modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.enable = true;
     powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
     open = false;
-
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-  hardware.nvidia.prime = {
-    #sync.enable = true; # sync mode, gpu will stay active
-    offload = {
-      enable = true;
-      enableOffloadCmd = true;
+    prime = {
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
     };
-    # Make sure to use the correct Bus ID values for your system!
-    intelBusId = "PCI:0:2:0";
-    nvidiaBusId = "PCI:1:0:0";
   };
-
-  boot.kernelParams = [ "nvidia-drm.modeset=1" ];
-
-environment.variables = {
-  __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-  LIBVA_DRIVER_NAME        = "nvidia";
-  WLR_NO_HARDWARE_CURSORS  = "1";   # fixes white boxes/flicker
-};
-
 }
