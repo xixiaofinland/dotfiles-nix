25e3cefb4caae5072910ee148e522c5e6ad2a3ee
xi xiao <tdxiaoxi2@gmail.com>
2025-07-30 10:11:29 +0300

    +

--- dotfiles/hypr/random_wallpaper.sh
+++ dotfiles/hypr/random_wallpaper.sh
@@ -2,6 +2,14 @@
 WALL_DIR="$HOME/Wallpapers"
 MONITOR="DP-2"
 
+# Wait for hyprpaper IPC to be ready (max ~5 seconds)
+for i in {1..10}; do
+  if hyprctl hyprpaper preload /dev/null 2>/dev/null; then
+    break
+  fi
+  sleep 0.5
+done
+
 # Pick a random wallpaper (follow symlinks)
 WALL=$(find -L "$WALL_DIR" -type f \( -iname '*.jpg' -o -iname '*.png' \) | shuf -n 1)
 
