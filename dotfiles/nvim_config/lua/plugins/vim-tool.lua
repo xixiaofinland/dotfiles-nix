861ec2f941e27917f578203757b73380db7bb646
xi xiao <tdxiaoxi2@gmail.com>
2025-08-30 09:02:05 +0300

    +

--- dotfiles/nvim_config/lua/plugins/vim-tool.lua
+++ dotfiles/nvim_config/lua/plugins/vim-tool.lua
@@ -1,19 +1,2 @@
 return {
-  {
-    "m4xshen/hardtime.nvim",
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    opts = {
-      disable_mouse = false,
-      disabled_keys = {
-        ["<Up>"] = false,
-        ["<Down>"] = false,
-        ["<Left>"] = false,
-        ["<Right>"] = false,
-      },
-    }
-  },
-
-  vim.keymap.set('n', '\\h', function()
-    vim.cmd('Hardtime toggle')
-  end, { noremap = true, silent = true, desc = 'hard-time' }),
 }
