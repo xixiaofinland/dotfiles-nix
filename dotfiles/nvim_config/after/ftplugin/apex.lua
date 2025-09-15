781e4608bf4a51c693770b153d9b53c25743f25f
xi xiao <tdxiaoxi2@gmail.com>
2025-09-15 08:28:17 +0300

    +

--- dotfiles/nvim_config/after/ftplugin/apex.lua
+++ dotfiles/nvim_config/after/ftplugin/apex.lua
@@ -1,1 +1,7 @@
 vim.opt_local.textwidth = 0 -- legacy code has long width and doesn't use prettier
+
+-- Enable treesitter-context automatically for apex
+require("treesitter-context").enable()
+
+-- Disable diagnostics in Apex
+vim.diagnostic.enable(false)
