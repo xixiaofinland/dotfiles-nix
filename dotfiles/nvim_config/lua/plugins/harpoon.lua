4a2ae94c8bb7d5b02860b8afe851e668acda0692
xi xiao <tdxiaoxi2@gmail.com>
2025-10-06 22:09:18 +0300

    +

--- dotfiles/nvim_config/lua/plugins/harpoon.lua
+++ dotfiles/nvim_config/lua/plugins/harpoon.lua
@@ -27,6 +27,27 @@ return {
     vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
     vim.keymap.set("n", "<C-t>", function() harpoon:list():select(4) end)
     vim.keymap.set("n", "<C-g>", function() harpoon:list():select(5) end)
-    vim.keymap.set("n", "<C-m>", function() harpoon:list():select(6) end)
+    -- vim.keymap.set("n", "<C-m>", function() harpoon:list():select(6) end)
+
+    -- Smart vsplit function
+    local function smart_vsplit_select(index)
+      local wins = vim.api.nvim_tabpage_list_wins(0)
+
+      if #wins > 1 then
+        vim.cmd('wincmd l')
+      else
+        vim.cmd('vsplit')
+      end
+
+      harpoon:list():select(index)
+    end
+
+    -- Smart vsplit with Ctrl+w then key
+    vim.keymap.set("n", "<C-w>p", function() smart_vsplit_select(1) end)
+    vim.keymap.set("n", "<C-w>d", function() smart_vsplit_select(2) end)
+    vim.keymap.set("n", "<C-w>n", function() smart_vsplit_select(3) end)
+    vim.keymap.set("n", "<C-w>u", function() smart_vsplit_select(4) end)
+    vim.keymap.set("n", "<C-w>g", function() smart_vsplit_select(5) end)
+    vim.keymap.set("n", "<C-w>m", function() smart_vsplit_select(6) end)
   end
 }
