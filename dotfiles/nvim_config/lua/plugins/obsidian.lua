3ebb79759866c1218bfd445976a06f2a48eb51ab
xi xiao <tdxiaoxi2@gmail.com>
2025-10-08 22:25:01 +0300

    +

--- dotfiles/nvim_config/lua/plugins/obsidian.lua
+++ dotfiles/nvim_config/lua/plugins/obsidian.lua
@@ -37,9 +37,13 @@ return {
       enable = false,
       -- update_debounce = 200,
       -- max_file_length = 5000,
+      checkboxes = {
+        [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+        ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+      },
     },
 
-    legacy_commands = false
+    legacy_commands = false,
   },
 
   config = function(_, opts)
