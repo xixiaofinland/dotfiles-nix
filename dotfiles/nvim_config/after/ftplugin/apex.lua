8a91e1425c988834f2f802319596c6a58429503b
xi xiao <tdxiaoxi2@gmail.com>
2025-10-08 18:41:39 +0300

    +

--- dotfiles/nvim_config/after/ftplugin/apex.lua
+++ dotfiles/nvim_config/after/ftplugin/apex.lua
@@ -5,12 +5,3 @@ require("treesitter-context").enable()
 
 -- Disable diagnostics in Apex
 vim.diagnostic.enable(false)
-
--- Jump to function names
-vim.keymap.set('n', '[m', function()
-  require 'nvim-treesitter.textobjects.move'.goto_previous_start("@function.name")
-end, { buffer = true, desc = "ts: prev function name" })
-
-vim.keymap.set('n', ']m', function()
-  require 'nvim-treesitter.textobjects.move'.goto_next_start("@function.name")
-end, { buffer = true, desc = "ts: next function name" })
