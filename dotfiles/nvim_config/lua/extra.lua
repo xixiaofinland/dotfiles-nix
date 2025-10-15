e3c046df21fca08c0f8b795f435f070c6318e787
xi xiao <tdxiaoxi2@gmail.com>
2025-10-15 21:24:37 +0300

    +

--- dotfiles/nvim_config/lua/extra.lua
+++ dotfiles/nvim_config/lua/extra.lua
@@ -65,3 +65,32 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.keymap.set('n', '<CR>', '<CR>', { buffer = event.buf, remap = false })
   end,
 })
+
+
+-- use "dd" to remove the current item in quickfix
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "qf",
+  callback = function(event)
+    vim.keymap.set("n", "dd", function()
+      local qflist = vim.fn.getqflist()
+      if #qflist == 0 then
+        return
+      end
+
+      local line = vim.fn.line(".")
+      table.remove(qflist, line)
+      vim.fn.setqflist({}, " ", { items = qflist })
+
+      local new_count = #qflist
+
+      if new_count > 0 then
+        local new_line = math.min(line, new_count)
+        vim.api.nvim_win_set_cursor(0, { new_line, 0 })
+      end
+    end, {
+      buffer = event.buf,
+      silent = true,
+      desc = "Delete entry in the quickfix list",
+    })
+  end,
+})
