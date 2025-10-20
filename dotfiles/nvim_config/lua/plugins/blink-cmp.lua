4cf0041d945b3205eb1821e746a224d25bbe458b
xi xiao <tdxiaoxi2@gmail.com>
2025-10-20 21:20:23 +0300

    +

--- dotfiles/nvim_config/lua/plugins/blink-cmp.lua
+++ dotfiles/nvim_config/lua/plugins/blink-cmp.lua
@@ -31,62 +31,31 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'dictionary', 'copilot' },
+      -- per_filetype = {
+      --   markdown = { "path", "buffer", "dictionary", "copilot" },
+      --   text = { "path", "buffer", "dictionary", "copilot" }
+      -- },
       providers = {
         buffer = {
-          score_offset = 0, -- increase, default was -3
+          score_offset = -1, -- increase, default was -3
         },
         copilot = {
           name = "copilot",
           module = "blink-copilot",
           async = true,
         },
-        thesaurus = {
-          name = "blink-cmp-words",
-          module = "blink-cmp-words.thesaurus",
-          opts = {
-            -- A score offset applied to returned items.
-            -- By default the highest score is 0 (item 1 has a score of -1, item 2 of -2 etc..).
-            score_offset = 0,
-
-            -- Default pointers define the lexical relations listed under each definition,
-            -- see Pointer Symbols below.
-            -- Default is as below ("antonyms", "similar to" and "also see").
-            definition_pointers = { "!", "&", "^" },
-
-            -- The pointers that are considered similar words when using the thesaurus,
-            -- see Pointer Symbols below.
-            -- Default is as below ("similar to", "also see" }
-            similarity_pointers = { "&", "^" },
-
-            -- The depth of similar words to recurse when collecting synonyms. 1 is similar words,
-            -- 2 is similar words of similar words, etc. Increasing this may slow results.
-            similarity_depth = 2,
-          },
-        },
-
-        -- Use the dictionary source
         dictionary = {
           name = "blink-cmp-words",
           module = "blink-cmp-words.dictionary",
-          -- All available options
           opts = {
             -- The number of characters required to trigger completion.
             -- Set this higher if completion is slow, 3 is default.
             dictionary_search_threshold = 3,
-
-            -- See above
-            score_offset = 0,
-
-            -- See above
+            score_offset = -5,
             definition_pointers = { "!", "&", "^" },
           },
         },
-      },
-      -- Setup completion by filetype
-      per_filetype = {
-        text = { "dictionary" },
-        markdown = { "thesaurus" },
       },
     },
 
