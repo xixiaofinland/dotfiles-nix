5bce97aa79d920e5c47e56e31aae151e864dbbc8
xi xiao <tdxiaoxi2@gmail.com>
2025-10-24 20:53:30 +0300

    +

--- dotfiles/nvim_config/lua/plugins/blink-cmp.lua
+++ dotfiles/nvim_config/lua/plugins/blink-cmp.lua
@@ -57,6 +57,31 @@ return {
           },
         },
       },
+      menu = {
+        -- use mini icons
+        draw = {
+          components = {
+            kind_icon = {
+              text = function(ctx)
+                local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+                return kind_icon
+              end,
+              -- (optional) use highlights from mini.icons
+              highlight = function(ctx)
+                local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                return hl
+              end,
+            },
+            kind = {
+              -- (optional) use highlights from mini.icons
+              highlight = function(ctx)
+                local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                return hl
+              end,
+            }
+          }
+        }
+      }
     },
 
     -- Disable cmdline
