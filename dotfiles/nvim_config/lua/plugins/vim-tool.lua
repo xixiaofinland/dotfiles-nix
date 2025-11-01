0d2b627fcd0edd8300a8d2b384c11b2f3ae77d36
xi xiao <tdxiaoxi2@gmail.com>
2025-11-01 17:49:09 +0200

    +

--- dotfiles/nvim_config/lua/plugins/vim-tool.lua
+++ dotfiles/nvim_config/lua/plugins/vim-tool.lua
@@ -1,2 +1,32 @@
 return {
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {
+      disable_mouse = false,
+      -- don't disable arrows
+      disabled_keys = {
+        ["<Up>"] = {},
+        ["<Down>"] = {},
+        ["<Left>"] = {},
+        ["<Right>"] = {},
+      },
+
+      -- define which keys get repetition penalty (but are allowed)
+      restricted_keys = {
+        ["<Up>"] = { "n", "x" },
+        ["<Down>"] = { "n", "x" },
+        ["<Left>"] = { "n", "x" },
+        ["<Right>"] = { "n", "x" },
+        ["h"] = { "n", "x" },
+        ["j"] = { "n", "x" },
+        ["k"] = { "n", "x" },
+        ["l"] = { "n", "x" },
+      },
+    }
+  },
+
+  vim.keymap.set('n', '\\h', function()
+    vim.cmd('Hardtime toggle')
+  end, { noremap = true, silent = true, desc = 'hard-time' }),
 }
