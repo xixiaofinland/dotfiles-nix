7232546a2f575f4ba5441e5648945e067495eaa3
xi xiao <xi.xiao007@gmail.com>
2026-02-11 00:57:27 +0200

    +

--- modules/home-core.nix
+++ modules/home-core.nix
@@ -41,12 +41,13 @@ in {
     lua-language-server
 
     # AI coding
-    github-copilot-cli # can't use its node package as it's dynamic links which Nix doesn't support
 
     # note: these are not managed by flake, as their version moves too fast :D
     # npm i -g @salesforce/cli
+
     # npm i -g @openai/codex
     # npm i -g @anthropic-ai/claude-code
+    # npm i -g @github/copilot
     # npm i -g opencode-ai
   ];
 
